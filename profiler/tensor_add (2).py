63bb5241c8f9fa1445e1e86ee76b3ff86b166b22
Emily Webber <egwebber@amazon.com>
2025-08-22 18:33:05 -0400

    Add files via upload

---
+++ profiler/tensor_add (2).py
@@ -0,0 +1,73 @@
+from neuronxcc import nki
+import neuronxcc.nki.language as nl
+
+import torch_xla.debug.profiler as xp
+
+import torch
+from torch_xla.core import xla_model as xm
+
+import torch_neuronx
+from torch_neuronx.experimental import profiler
+import os
+
+os.environ["NEURON_CC_FLAGS"] = "--cache_dir=./compiler_cache"
+
+@nki.jit
+def nki_tensor_add_kernel(a_input, b_input):
+    """NKI kernel to compute element-wise addition of two input tensors
+    """
+
+    c_output = nl.ndarray(a_input.shape, dtype=a_input.dtype, buffer=nl.shared_hbm)
+
+    # Check all input/output tensor shapes are the same for element-wise operation
+    assert a_input.shape == b_input.shape
+
+    # Check size of the first dimension does not exceed on-chip memory tile size limit,
+    # so that we don't need to tile the input to keep this example simple
+    assert a_input.shape[0] <= nl.tile_size.pmax
+
+    # Load the inputs from device memory to on-chip memory
+    a_tile = nl.load(a_input)
+    b_tile = nl.load(b_input)
+
+    # Specify the computation (in our case: a + b)
+    c_tile = nl.add(a_tile, b_tile)
+
+    # Store the result to c_output from on-chip memory to device memory
+    nl.store(c_output, value=c_tile)
+
+    return c_output
+
+
+if __name__ == "__main__":
+    
+    # XLA: Specify XLA device (defaults to a NeuronCore on Trn1 instance)
+    device = xm.xla_device()
+    
+    # Start the profiler context-manager
+    with torch_neuronx.experimental.profiler.profile(
+        port=9012,
+        profile_type='system',
+        target='neuron_profile_perfetto',
+        output_dir=os.environ['NEURON_RT_INSPECT_OUTPUT_DIR'],
+        ms_duration=30000) as profiler:
+
+        num_runs = 10
+
+        result = 0
+        
+        for i in range(1, num_runs):
+            
+            a = torch.ones((4, i), dtype=torch.float16).to(device=device)
+            b = torch.ones((4, i), dtype=torch.float16).to(device=device)
+
+            c = nki_tensor_add_kernel(a, b)
+            
+            d = torch.ones((4, i), dtype=torch.float16).to(device=device)
+            e = torch.ones((4, i), dtype=torch.float16).to(device=device)
+
+            f = nki_tensor_add_kernel(d, e)
+
+            out = c + f
+
+        print (out)
