64d5ddb4b1e345365c91f18c8481201c4375a63c
Emily Webber <egwebber@amazon.com>
2025-09-03 12:31:08 -0400

    Update moe_fwd.py

--- moe/moe_fwd.py
+++ moe/moe_fwd.py
@@ -1,16 +1,35 @@
+'''
+This is a 12-step tutorial that shows how to iteratively develop and improve a NKI kernels for mixture of experts. We focus on the MLP contraction for forward pass, and specifically the context encoding part. We use a TP-degree of 4, assuming a single Trn2 chip which has a default of 4 logical neuron cores. We'll also assume an input context length of 128K, and an output sequence length of 4096.
+
+For simplicity, we'll start with a much smaller context lenghth of only 128 tokens. Then we'll work up to the larger context length throughout the tutorial. We'll also start without the router, adding this later in the tutorial.
+'''
+
 import numpy as np
 
 ###############################
 # v1 - write MOE fwd in Numpy #
 ###############################
-def v1():
+def v1(t, mlp1_weight, mlp1_bias, mlp2_weight, mlp2_bias):
+    '''
+    Input tensors:
+        t = (128, 2880)
+        mlp1_weight = (8, 1440, 2880)
+        mlp1_bias = (8, 1440)
+        mlp2_weight = (8, 2880, 720)
+        mlp2_bias = (8, 2880)
 
-    # takes a token, mlp1_weight, mlp1_bias, mlp2_weight, mlp2_bias
-    
+    Output tensor:
+        t_out = (128, 2880)
+        
+    '''
+
+    # just the rmsnorm
     # t = norm(t)
 
+    # a linear transformation
     # g = gate(t)
 
+    # for numpy, use np.argsort probably
     # experts = topk(g)
     
     # expert_weights = softmax(experts)
@@ -28,18 +47,38 @@ def v1():
     
     # t = torch.einsum("bec,be->bc", t, expert_weights)
     
-    return
+    return t
 
+def generate_input_shapes(tp=4, context_length = 128000, hidden_size = 2880, num_experts = 32):
 
+    intermediate_size_per_device = (hidden_size * 2) // tp
+
+    experts_per_device = num_experts // tp 
+    
+    t = np.random.randn(context_length, hidden_size).astype(np.float16)
+    
+    mlp1_weight = np.random.randn(experts_per_device, intermediate_size_per_device, hidden_size).astype(np.float16)
+    
+    mlp1_bias = np.random.randn(experts_per_device, intermediate_size_per_device).astype(np.float16)
+
+    mlp2_weight = np.random.randn(experts_per_device, hidden_size,  hidden_size // tp).astype(np.float16)
+
+    mlp2_bias = np.random.randn(experts_per_device, hidden_size).astype(np.float16)
+
+    return t, mlp1_weight, mlp1_bias, mlp2_weight, mlp2_bias
 
 
 
 def main(version):
+    
     if 'numpy' in version:
-        v1()
 
+        # call generate shapes for this version 
+        t, mlp1_weight, mlp1_bias, mlp2_weight, mlp2_bias = generate_input_shapes(context_length = 128)
+        
+        t_out = v1(t, mlp1_weight, mlp1_bias, mlp2_weight, mlp2_bias)
 
-
+        assert t.shape == t_out.shape
 
 if __name__ == "__main__":
     main(version = 'numpy')
