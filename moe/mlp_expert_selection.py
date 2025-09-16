64e7d94e69d3164d32621927e03e22940f62714a
Emily Webber <egwebber@amazon.com>
2025-09-16 11:33:47 -0400

    Moved reshape to topk, dropped extra DMAs

--- moe/mlp_expert_selection.py
+++ moe/mlp_expert_selection.py
@@ -48,6 +48,35 @@ def nki_isa_topk(g, k=4, TILE_P=128):
     
     return expert_indices, expert_values
 
+def nki_isa_topk_reshape(g, k):
+    """
+    Compute top-k values and indices using NKI ISA operations with efficient reshaping
+    """
+    TILE_P = 128
+    
+    # Initialize output arrays (using 128 on the 1st dim only for broadcasting rules)
+    expert_indices_XL = nl.ndarray((nl.par_dim(TILE_P), TILE_P, k), dtype=np.float32, buffer=nl.sbuf)
+
+    expert_indices = nl.ndarray((nl.par_dim(1), TILE_P, k), dtype=np.float32, buffer=nl.sbuf)
+    
+    expert_values = nisa.memset(shape=[TILE_P, k], value=0, dtype=g.dtype)
+    
+    # Find top 8 values and indices
+    top_vals = nisa.max8(src=g)
+    top_indices = nisa.nc_find_index8(data=g, vals=top_vals)
+    
+    # Copy values
+    for e in nl.static_range(k):
+        expert_values[:, e] = top_vals[:, e]
+
+        # works
+        expert_indices_XL[0:TILE_P, 0:TILE_P, e:e+1] = top_indices[0:TILE_P, e:e+1]
+
+        # breaks on expanding paramter b in assignment from (128, 1) to (128, 1, 1)
+        # expert_indices[0:1, 0:TILE_P, e:e+1] = top_indices[0:TILE_P, e:e+1]
+
+    return expert_indices_XL, expert_values
+
 
 @nki.jit(debug_kernel=True)
 def sample_selection_kernel(t, scale, gate_weight, gate_bias, mlp1_weight, mlp1_bias, mlp2_weight, mlp2_bias):
@@ -88,17 +117,27 @@ def sample_selection_kernel(t, scale, gate_weight, gate_bias, mlp1_weight, mlp1_
     g = nl.matmul(t, gate_weight)
     g = nl.add(g, gate_bias)
 
+    ##########################
+    # Works at high DMA cost #
+    ##########################
+
     # topk    
-    expert_indices, expert_values = nki_isa_topk(g, k) # (128, 4)
-
-    # works with indices on hbm, doesn't work with assignment on sbuf, but indirect indices tiles must be on sbuf per error statement, so sending back down to sbuf to use block dim properly
-    experts_indices_hbm = nl.ndarray((nl.par_dim(1), batch_size, k), dtype = expert_indices.dtype, buffer = nl.hbm)
-
-    # inefficient but working loop from sbuf -> hbm -> sbuf
-    nl.store(experts_indices_hbm[0, 0:128, 0:4:], expert_indices[0:128, 0:4])
-    expert_indices_sbuf = nl.ndarray((nl.par_dim(1), batch_size, k), dtype = expert_indices.dtype, buffer = nl.sbuf)
-    expert_indices_sbuf[0:1, :, :] = nl.load(experts_indices_hbm[0:1, :, :])
+    # expert_indices, expert_values = nki_isa_topk(g, k) # (128, 4)
     
+    # works with indices on hbm, doesn't work with assignment on sbuf, but indirect indices tiles must be on sbuf per error statement, so sending back down to sbuf to use block dim properly
+    # experts_indices_hbm = nl.ndarray((nl.par_dim(1), batch_size, k), dtype = expert_indices.dtype, buffer = nl.hbm)
+
+    # # inefficient but working loop from sbuf -> hbm -> sbuf
+    # nl.store(experts_indices_hbm[0, 0:128, 0:4:], expert_indices[0:128, 0:4])
+    # expert_indices_sbuf = nl.ndarray((nl.par_dim(1), batch_size, k), dtype = expert_indices.dtype, buffer = nl.sbuf)
+    # expert_indices_sbuf[0:1, :, :] = nl.load(experts_indices_hbm[0:1, :, :])
+
+    #################################
+    # Works with higher memory cost #
+    #################################
+    
+    expert_indices_sbuf, expert_values = nki_isa_topk_reshape(g, k) # (128, 4)
+
     selected_weights = nl.ndarray((batch_size, k, nl.par_dim(intermediate_size), hidden_size), dtype=mlp1_weight.dtype, buffer=nl.sbuf)
     
     for b in nl.static_range(batch_size):
@@ -125,3 +164,4 @@ if __name__ == "__main__":
 
     out = sample_selection_kernel(t, scale, gate_weight, gate_bias, mlp1_weight, mlp1_bias, mlp2_weight, mlp2_bias) # returns shape (64,128)
 
+    breakpoint()
